d8c95b7e6c652fb54a21869cb062a62cee423932
pameljoel <pj1986@gmail.com>
2019-01-21 21:21:29 +0100

    removed dependecies:     "gatsby-plugin-sharp": "^2.0.14",     "gatsby-source-filesystem": "^2.0.8",     "gatsby-transformer-sharp": "^2.1.8",
    added .eslintrc

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,15 +8,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
