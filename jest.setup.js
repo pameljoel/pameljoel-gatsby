626ee550f828a79c592823b7e9830daf08f1c851
jbeltre <joel.beltre@lastminute.com>
2019-11-15 19:25:21 +0100

    added matchmedia jest mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,20 @@
-import Enzyme, { mount, render, shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Enzyme, { mount, render, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() })
 
 // Make Enzyme functions available in all test files without importing
-global.shallow = shallow
-global.render = render
-global.mount = mount
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+});
