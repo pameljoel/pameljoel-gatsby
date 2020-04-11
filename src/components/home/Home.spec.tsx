7aa1852f2ad4ce5cdd20c8bf3e2104b9b145fd1e
jbeltre <joel.beltre@lastminute.com>
2020-04-11 21:28:25 +0200

    added tests to Home

--- src/components/home/Home.spec.tsx
+++ src/components/home/Home.spec.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Home from './Home';
+import HomeStaticContent from './HomeStaticContent';
+import Loading from '../status/Loading';
 
 describe('Home', () => {
-  const wrapper = shallow(<Home />);
-  const search = (node: any) => {
-    return wrapper.find(node);
+  const wrapper = mount(<Home />);
+
+  const searchSelector = (selector: string) => {
+    return wrapper.find(selector);
+  };
+
+  const searchSelectorInComponent = (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    component: React.JSXElementConstructor<any>,
+    selector: string
+  ) => {
+    const deepWrapper = mount(<Home />);
+
+    const staticContent = deepWrapper.find(component);
+    return staticContent.find(selector);
   };
 
   it('renders without crashing', () => {
-    expect(wrapper.length).toBe(0);
-    const selector = '[data-test="home"]';
-    expect(search(selector)).toBeDefined();
+    expect(wrapper.length).toBe(1);
   });
 
-  it('renders the static content', () => {});
+  it('renders DOM', () => {
+    const selector = '[data-test="home"]';
+    expect(searchSelector(selector).length).toBe(1);
+  });
+
+  it('renders static content', () => {
+    const selector = '[data-test="static"]';
+    const Component = searchSelectorInComponent(HomeStaticContent, selector);
+
+    expect(Component.length).toBe(1);
+  });
+
+  describe('loading', () => {
+    const selector = '[data-test="loading"]';
+    const Component = searchSelectorInComponent(Loading, selector);
+
+    expect(Component.length).toBe(0);
+  });
 });
