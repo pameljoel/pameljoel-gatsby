3ae51d897685e14be82e77c20485ffe6a821163f
jbeltre <joel.beltre@lastminute.com>
2020-04-12 16:31:33 +0200

    RelatedProjects small refactor, and style for mobile and focus

--- src/components/carousel/RelatedProjects.js
+++ src/components/carousel/RelatedProjects.js
@@ -1,45 +1,72 @@
-import React, { Component } from 'react'
-import Slider from 'react-slick'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import PropTypes from 'prop-types';
 
-import RelatedProject from './RelatedProject'
-import CloseButton from './../buttons/CloseButton'
+import RelatedProject from './RelatedProject';
+import CloseButton from './../buttons/CloseButton';
 
-import './relatedProjects.scss'
+import './relatedProjects.scss';
+
+const settings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+};
+
+const ResultsHeader = (props) => {
+  const { hasResults, callback, selected } = props;
+  const resultsText = `Featured projects with `;
+  const noResultsText = `There are no featured projects at the moment with `;
+  const text = hasResults ? resultsText : noResultsText;
+
+  return (
+    <header>
+      <CloseButton callback={callback} />
+      {text} <strong>{selected}</strong>
+    </header>
+  );
+};
 
 export default class RelatedProjects extends Component {
   render() {
-    const settings = {
-      dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 3,
-    }
     const {
       projects,
       selected,
       emptyFilteredProjects,
       setSelectedProject,
-    } = this.props
+    } = this.props;
     return (
       <div className="related-projects">
         {selected && projects.length === 0 && (
-          <div className="related-project-no-result">
-            <CloseButton callback={emptyFilteredProjects} />
-            There are no featured projects at the moment with{' '}
-            <strong>{selected}</strong>
+          <div className="related-projects__result">
+            <ResultsHeader
+              hasResults={false}
+              selected={selected}
+              callback={emptyFilteredProjects}
+            />
           </div>
         )}
 
         {selected && projects.length > 0 && (
-          <div className="related-projects-result">
-            <CloseButton callback={emptyFilteredProjects} />
-            <p>
-              Projects with <strong>{selected}</strong>
-            </p>
+          <div className="related-projects__result">
+            <ResultsHeader
+              hasResults={true}
+              selected={selected}
+              callback={emptyFilteredProjects}
+            />
             <Slider {...settings}>
               {projects.map((project, i) => {
-                const { slug } = project
+                const { slug } = project;
                 return (
                   <div key={slug}>
                     <RelatedProject
@@ -48,13 +75,13 @@ export default class RelatedProjects extends Component {
                       setSelectedProject={setSelectedProject}
                     />
                   </div>
-                )
+                );
               })}
             </Slider>
           </div>
         )}
       </div>
-    )
+    );
   }
 }
 
@@ -63,10 +90,10 @@ RelatedProjects.propTypes = {
   selected: PropTypes.string,
   emptyFilteredProjects: PropTypes.func,
   setSelectedProject: PropTypes.func,
-}
+};
 
 RelatedProjects.defaultProps = {
   projects: [],
   selected: null,
-  setSelectedProject: null
-}
+  setSelectedProject: null,
+};
