403ca5e3afb185ce958f7ab6a62c5e3b70689964
jbeltre <joel.beltre@lastminute.com>
2020-04-17 19:02:52 +0200

    mobile spacing adjustments

--- src/components/curriculum/Curriculum.js
+++ src/components/curriculum/Curriculum.js
@@ -13,6 +13,38 @@ import careerJson from '../../../static/resources/career.json';
 
 import './curriculum.scss';
 
+const StaticHeader = () => {
+  return (
+    <header
+      className="big-header"
+      style={{ marginBottom: '0', paddingBottom: '100px' }}
+    >
+      <div className="big-header-content">
+        <h1 className="">CV</h1>
+        <div className="subtitle">My Curriculum Vitae</div>
+      </div>
+      <div className="big-header-background" />
+    </header>
+  );
+};
+
+const Content = (props) => {
+  const { isLoading, personal, career, education, setSelectedProject } = props;
+  return (
+    <div className="container">
+      {!isLoading && (personal || career || education) ? (
+        <div className="curriculum-container">
+          <Section data={personal} />
+          <Career data={career} setSelectedProject={setSelectedProject} />
+          <Education data={education} />
+        </div>
+      ) : (
+        <Loading isLoading={isLoading} />
+      )}
+    </div>
+  );
+};
+
 export default class Curriculum extends Component {
   constructor(props) {
     super(props);
@@ -52,27 +84,14 @@ export default class Curriculum extends Component {
     const { isLoading, personal, career, education } = this.state;
     return (
       <div>
-        <header
-          className="big-header"
-          style={{ marginBottom: '0', paddingBottom: '100px' }}
-        >
-          <div className="big-header-content">
-            <h1 className="">CV</h1>
-            <div className="subtitle">My Curriculum Vitae</div>
-          </div>
-          <div className="big-header-background" />
-        </header>
-        <div className="container">
-          {!isLoading && (personal || career || education) ? (
-            <div className="curriculum-container">
-              <Section data={personal} />
-              <Career data={career} setSelectedProject={setSelectedProject} />
-              <Education data={education} />
-            </div>
-          ) : (
-            <Loading isLoading={isLoading} />
-          )}
-        </div>
+        <StaticHeader />
+        <Content
+          isLoading={isLoading}
+          personal={personal}
+          career={career}
+          education={education}
+          setSelectedProject={setSelectedProject}
+        />
       </div>
     );
   }
