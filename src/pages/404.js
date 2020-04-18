1562c8576df956950499878bce43c391c0a8237c
jbeltre <joel.beltre@lastminute.com>
2020-04-18 09:52:48 +0200

    removed space

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,19 +1,18 @@
-import React, { Fragment } from 'react'
-import SEO from '../components/seo'
+import React, { Fragment } from 'react';
+import SEO from '../components/seo';
 
 const NotFoundPage = () => (
   <Fragment>
-
-    <div className="card" >
+    <div className="card">
       <div className="error-panel">
         <SEO title="404: Not found" />
         <h1 className="error-code">NOT FOUND</h1>
         <div className="error-message">
           You just hit a route that doesn&#39;t exist... the sadness.
-          </div>
+        </div>
       </div>
     </div>
   </Fragment>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
