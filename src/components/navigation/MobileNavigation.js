3b6bce6418ab2b013a895dc61a80f24e8e97bdc6
jbeltre <joel.beltre@lastminute.com>
2020-04-25 23:27:02 +0200

    refactor with hooks

--- src/components/navigation/MobileNavigation.js
+++ src/components/navigation/MobileNavigation.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Link from 'gatsby-link';
 
 import { openCrisp } from '../crisp/Crisp';
@@ -7,67 +7,52 @@ import Hamburger from './Hamburger';
 
 import './mobileNavigation.scss';
 
-export default class MobileNavigation extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sidebarOpen: false,
-    };
-    this.toggleSidebar = this.toggleSidebar.bind(this);
-  }
+const MobileNavigation = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  toggleSidebar() {
-    this.setState((prevState) => ({ sidebarOpen: !prevState.sidebarOpen }));
-  }
+  const toggleMenu = () => {
+    setMenuOpen((prevState) => !prevState);
+  };
 
-  render() {
-    const { sidebarOpen } = this.state;
+  const ListItem = (props) => {
+    const { text, to } = props;
     return (
-      <Fragment>
-        <div className={`mobile-navigation ${sidebarOpen ? 'open' : ''}`}>
-          <Hamburger open={sidebarOpen} callback={this.toggleSidebar} />
-          <ul className="mobile-navigation-links">
-            <li onClick={this.toggleSidebar}>
-              <Link to="/" activeClassName="active">
-                <div className="nav-link">Home</div>
-                <div className="active-bar" />
-              </Link>
-            </li>
-            <li onClick={this.toggleSidebar}>
-              <Link to="/daily" activeClassName="active">
-                <div className="nav-link">Daily</div>
-                <div className="active-bar" />
-              </Link>
-            </li>
-            <li onClick={this.toggleSidebar}>
-              <Link to="/curriculum" activeClassName="active">
-                <div className="nav-link">CV</div>
-                <div className="active-bar" />
-              </Link>
-            </li>
-            <li onClick={this.toggleSidebar}>
-              <Link to="/projects" activeClassName="active">
-                <div className="nav-link">Projects</div>
-                <div className="active-bar" />
-              </Link>
-            </li>
-            <li
-              onClick={() => {
-                this.toggleSidebar();
-                openCrisp();
-              }}
-            >
-              <button className="nav-button">Contact me</button>
-            </li>
-          </ul>
-          <Branding />
-        </div>
-        <div
-          className={`overlay-layer ${sidebarOpen ? 'open' : ''}`}
-          key="overlay-layer"
-          onClick={this.toggleSidebar}
-        />
-      </Fragment>
+      <li onClick={toggleMenu}>
+        <Link to={to} activeClassName="active">
+          <div className="nav-link">{text}</div>
+          <div className="active-bar" />
+        </Link>
+      </li>
     );
-  }
-}
+  };
+
+  return (
+    <Fragment>
+      <div className={`mobile-navigation ${menuOpen ? 'open' : ''}`}>
+        <Hamburger open={menuOpen} callback={toggleMenu} />
+        <ul className="mobile-navigation-links">
+          <ListItem to="/" text="Home" />
+          <ListItem to="/daily" text="Daily" />
+          <ListItem to="/curriculum" text="C.V." />
+          <ListItem to="/projects" text="Projects" />
+          <li
+            onClick={() => {
+              toggleMenu();
+              openCrisp();
+            }}
+          >
+            <button className="nav-button">Contact me</button>
+          </li>
+        </ul>
+        <Branding />
+      </div>
+      <div
+        className={`overlay-layer ${menuOpen ? 'open' : ''}`}
+        key="overlay-layer"
+        onClick={toggleMenu}
+      />
+    </Fragment>
+  );
+};
+
+export default MobileNavigation;
