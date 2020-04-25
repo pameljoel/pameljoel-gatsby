198c7a24f7537c612f3d4d89f3a93a0874706b5d
jbeltre <joel.beltre@lastminute.com>
2020-04-26 00:18:00 +0200

    refactor with hooks

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,7 +1,6 @@
-import React, { Component, cloneElement, Children, Fragment } from 'react';
+import React, { cloneElement, Children, Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import * as Sentry from '@sentry/browser';
-import { ProjectContext } from '../Context';
 import Navigation from '../components/navigation/Navigation';
 import Footer from '../components/footer/Footer';
 import ScrollTop from '../components/scrollTop/ScrollTop';
@@ -10,39 +9,27 @@ Sentry.init({
   dsn: 'https://9ad1e40b5209458cb212458aad59cf3f@sentry.io/1779398',
 });
 
-export default class SharedState extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedProject: null,
-    };
-    this.setSelectedProject = this.setSelectedProject.bind(this);
-    this.removeSelectedProject = this.removeSelectedProject.bind(this);
-  }
-  setSelectedProject(number) {
-    this.setState({ selectedProject: number });
-  }
-  removeSelectedProject() {
-    this.setState({ selectedProject: null });
-  }
-  render() {
-    const publicAPI = {
-      selectedProject: this.state.selectedProject,
-      setSelectedProject: this.setSelectedProject,
-      removeSelectedProject: this.removeSelectedProject,
-    };
-    return (
-      <Fragment>
-        <Navigation />
-        <ScrollTop />
-        {cloneElement(Children.only(this.props.children), publicAPI)}
-        <Footer />
-      </Fragment>
-    );
-  }
-}
+const SharedState = (props) => {
+  const [selectedProject, setSelectedProject] = useState(null);
 
-SharedState.contextType = ProjectContext;
+  const removeSelectedProject = () => {
+    setSelectedProject(null);
+  };
+
+  const publicAPI = {
+    selectedProject: selectedProject,
+    setSelectedProject: setSelectedProject,
+    removeSelectedProject,
+  };
+  return (
+    <Fragment>
+      <Navigation />
+      <ScrollTop />
+      {cloneElement(Children.only(props.children), publicAPI)}
+      <Footer />
+    </Fragment>
+  );
+};
 
 SharedState.propTypes = {
   children: PropTypes.oneOfType([
@@ -50,3 +37,5 @@ SharedState.propTypes = {
     PropTypes.node,
   ]).isRequired,
 };
+
+export default SharedState;
