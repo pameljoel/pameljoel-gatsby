6fb8d1908142fa4e59a6fa012149039501065776
jbeltre <joel.beltre@lastminute.com>
2020-04-25 23:44:06 +0200

    refactor with hooks

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
 
@@ -6,77 +6,72 @@ import Loading from '../status/Loading';
 import { enableCrisp } from '../crisp/Crisp';
 import Project from './Project';
 
-import { getData } from '../../helpers';
+import { getDataAsync } from '../../helpers';
 import projectJson from '../../../static/resources/projects.json';
 
 import './projects.scss';
 import './../carousel/slick.scss';
 
-export default class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      projects: null,
-      isLoading: true,
-    };
+const Projects = (props) => {
+  const { selectedProject } = props;
+  const [projects, setProjects] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function getData() {
+    const projectsData = await getDataAsync(projectJson);
+    setProjects(projectsData);
+    setIsLoading(false);
   }
 
-  componentDidMount() {
+  useEffect(() => {
     enableCrisp();
-    getData(projectJson)
-      .then((data) => this.setState({ projects: data, isLoading: false }))
-      .catch((error) => {
-        this.setState({ isLoading: false });
-        console.error(error);
-      });
-  }
+    getData().catch((error) => {
+      setIsLoading(false);
+      console.error(error);
+    });
+  });
 
-  render() {
-    const { projects, isLoading } = this.state;
-    const settings = {
-      dots: false,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      adaptiveHeight: true,
-      initialSlide: this.props.selectedProject || 0,
-    };
-    return (
-      <div>
-        <header
-          className="big-header"
-          style={{ marginBottom: '-300px', paddingBottom: '200px' }}
-        >
-          <div className="big-header-content">
-            <h1 className="">PROJECTS</h1>
-            <div className="subtitle">
-              This is a selection of my web projects
-            </div>
-          </div>
-          <div className="big-header-background" />
-        </header>
-        <div className="projects-container">
-          {!isLoading && projects ? (
-            <div className="projects">
-              {projects.length > 0 && (
-                <Slider {...settings}>
-                  {projects.map((project) => (
-                    <div key={`project-slider-${project.slug}`}>
-                      <Project data={project} />
-                    </div>
-                  ))}
-                </Slider>
-              )}
-            </div>
-          ) : (
-            <Loading isLoading={isLoading} />
-          )}
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    initialSlide: selectedProject || 0,
+  };
+  return (
+    <div>
+      <header
+        className="big-header"
+        style={{ marginBottom: '-300px', paddingBottom: '200px' }}
+      >
+        <div className="big-header-content">
+          <h1 className="">PROJECTS</h1>
+          <div className="subtitle">This is a selection of my web projects</div>
         </div>
+        <div className="big-header-background" />
+      </header>
+      <div className="projects-container">
+        {!isLoading && projects ? (
+          <div className="projects">
+            {projects.length > 0 && (
+              <Slider {...settings}>
+                {projects.map((project) => (
+                  <div key={`project-slider-${project.slug}`}>
+                    <Project data={project} />
+                  </div>
+                ))}
+              </Slider>
+            )}
+          </div>
+        ) : (
+          <Loading isLoading={isLoading} />
+        )}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Projects.propTypes = {
   data: PropTypes.arrayOf(
@@ -100,3 +95,5 @@ Projects.propTypes = {
 Projects.defaultProps = {
   selectedProject: 0,
 };
+
+export default Projects;
