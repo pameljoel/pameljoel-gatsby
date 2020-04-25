9563b9a1ef89f2852de9f0d640802d8d208496af
jbeltre <joel.beltre@lastminute.com>
2020-04-25 09:52:12 +0200

    format

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,34 +1,36 @@
-import React, { Component, cloneElement, Children, Fragment } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component, cloneElement, Children, Fragment } from 'react';
+import PropTypes from 'prop-types';
 import * as Sentry from '@sentry/browser';
-import { ProjectContext } from '../Context'
-import Navigation from '../components/navigation/Navigation'
-import Footer from '../components/footer/Footer'
+import { ProjectContext } from '../Context';
+import Navigation from '../components/navigation/Navigation';
+import Footer from '../components/footer/Footer';
 import ScrollTop from '../components/scrollTop/ScrollTop';
 
-Sentry.init({dsn: "https://9ad1e40b5209458cb212458aad59cf3f@sentry.io/1779398"});
+Sentry.init({
+  dsn: 'https://9ad1e40b5209458cb212458aad59cf3f@sentry.io/1779398',
+});
 
 export default class SharedState extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       selectedProject: null,
-    }
-    this.setSelectedProject = this.setSelectedProject.bind(this)
-    this.removeSelectedProject = this.removeSelectedProject.bind(this)
+    };
+    this.setSelectedProject = this.setSelectedProject.bind(this);
+    this.removeSelectedProject = this.removeSelectedProject.bind(this);
   }
   setSelectedProject(number) {
-    this.setState({ selectedProject: number })
+    this.setState({ selectedProject: number });
   }
   removeSelectedProject() {
-    this.setState({ selectedProject: null })
+    this.setState({ selectedProject: null });
   }
   render() {
     const publicAPI = {
       selectedProject: this.state.selectedProject,
       setSelectedProject: this.setSelectedProject,
       removeSelectedProject: this.removeSelectedProject,
-    }
+    };
     return (
       <Fragment>
         <Navigation />
@@ -36,15 +38,15 @@ export default class SharedState extends Component {
         {cloneElement(Children.only(this.props.children), publicAPI)}
         <Footer />
       </Fragment>
-    )
+    );
   }
 }
 
-SharedState.contextType = ProjectContext
+SharedState.contextType = ProjectContext;
 
 SharedState.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]).isRequired,
-}
+};
