f57d8b380071c40d2536ea0083e136274d92e673
jbeltre <joel.beltre@lastminute.com>
2020-09-04 12:14:09 +0200

    functional component

--- src/components/navigation/Branding.js
+++ src/components/navigation/Branding.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Link from 'gatsby-link';
 
-export default class Branding extends Component {
-  render() {
-    return (
-      <Link className="nav-logo__container" to="/">
-        <div className="nav-logo"><strong>P</strong>amel <strong>J</strong>oel <strong>B</strong>eltrè <small> | Personal Portfolio site</small></div>
-      </Link>
-    );
-  }
-}
+const Branding = () => {
+  return (
+    <Link className="nav-logo__container" to="/">
+      <div className="nav-logo">
+        <strong>P</strong>amel <strong>J</strong>oel <strong>B</strong>eltrè{' '}
+        <small> | Personal Portfolio site</small>
+      </div>
+    </Link>
+  );
+};
+
+export default Branding;
