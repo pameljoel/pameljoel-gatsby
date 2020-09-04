919986abb5a50ea243f1397751b17eb88889d755
jbeltre <joel.beltre@lastminute.com>
2020-09-04 12:08:31 +0200

    functional component

--- src/components/curriculum/School.js
+++ src/components/curriculum/School.js
@@ -1,49 +1,48 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-import Tags from './../tags/Tags'
+import Tags from './../tags/Tags';
 
-import './school.scss'
-import './../card.scss'
+import './school.scss';
+import './../card.scss';
 
-export default class School extends Component {
-  render() {
-    const { data } = this.props
-    const { degree, degreeType, school, city, year, subjects } = data
-    return (
-      <article className="card school-container">
-        <header className="school-role">
-          <h1>{degree}</h1>
-        </header>
-        <div className="school-data-container">
-          <span className="school-degree-type">
-            <span className="text">{degreeType}</span>
-          </span>
+const School = ({ data }) => {
+  const { degree, degreeType, school, city, year, subjects } = data;
+  return (
+    <article className="card school-container">
+      <header className="school-role">
+        <h1>{degree}</h1>
+      </header>
+      <div className="school-data-container">
+        <span className="school-degree-type">
+          <span className="text">{degreeType}</span>
+        </span>
 
-          <span className="school-name">
-            <span className="prefix">at</span>
-            <span className="text">{school}</span>
-          </span>
+        <span className="school-name">
+          <span className="prefix">at</span>
+          <span className="text">{school}</span>
+        </span>
 
-          <span className="school-city">
-            <span className="prefix">in</span>
-            <span className="text">{city}</span>
-          </span>
+        <span className="school-city">
+          <span className="prefix">in</span>
+          <span className="text">{city}</span>
+        </span>
 
-          <span className="school-year">
-            <span className="prefix">year</span>
-            <span className="text">{year}</span>
-          </span>
-        </div>
+        <span className="school-year">
+          <span className="prefix">year</span>
+          <span className="text">{year}</span>
+        </span>
+      </div>
 
-        <div className="school-skills-container">
-          <Tags data={subjects} />
-        </div>
-        <div className="school-projects" />
-      </article>
-    )
-  }
-}
+      <div className="school-skills-container">
+        <Tags data={subjects} />
+      </div>
+      <div className="school-projects" />
+    </article>
+  );
+};
+
+export default School;
 
 School.propTypes = {
   data: PropTypes.shape({
@@ -54,4 +53,4 @@ School.propTypes = {
     year: PropTypes.string,
     subjects: PropTypes.arrayOf(PropTypes.string),
   }),
-}
+};
