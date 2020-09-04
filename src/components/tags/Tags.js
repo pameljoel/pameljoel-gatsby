6ff5cd5b9780470ea567659c280eac6db9735770
jbeltre <joel.beltre@lastminute.com>
2020-09-04 12:11:35 +0200

    functional component

--- src/components/tags/Tags.js
+++ src/components/tags/Tags.js
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
 import ObjectTag from './ObjectTag';
 import './tags.scss';
-import { StringTag } from './StringTag'
+import { StringTag } from './StringTag';
 
 export const isObject = (input) => typeof input === 'object';
 
-export default class Tags extends Component {
-  render() {
-    const { data: tags, handleClick } = this.props;
-    if (!tags) return null;
+const Tags = ({ data: tags, handleClick }) => {
+  if (!tags) return null;
 
-    return (
-      <div className="tags">
-        {tags.map((tag, i) => {
-          const { name, topSkill, newSkill } = tag;
-          return isObject(tag) ? (
-            <ObjectTag
-              name={name}
-              topSkill={topSkill}
-              newSkill={newSkill}
-              key={`tag-${i}`}
-              handleClick={handleClick}
-            />
-          ) : (
-            <StringTag name={tag} key={`tag-${i}`} handleClick={handleClick} />
-          );
-        })}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="tags">
+      {tags.map((tag, i) => {
+        const { name, topSkill, newSkill } = tag;
+        return isObject(tag) ? (
+          <ObjectTag
+            name={name}
+            topSkill={topSkill}
+            newSkill={newSkill}
+            key={`tag-${i}`}
+            handleClick={handleClick}
+          />
+        ) : (
+          <StringTag name={tag} key={`tag-${i}`} handleClick={handleClick} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Tags;
