a1b9c3c3dcb35dda04563ea6b77ad30e7038db38
jbeltre <joel.beltre@lastminute.com>
2020-09-04 12:16:40 +0200

    functional component

--- src/components/navigation/Hamburger.js
+++ src/components/navigation/Hamburger.js
@@ -2,20 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './hamburger.scss';
 
-export default class Hamburger extends Component {
-  render() {
-    const { open, callback } = this.props;
-    return (
-      <div role="button" aria-label="open sidebar" tabIndex="0" className={`hamburger ${open ? 'open' : ''}`}
-        onClick={callback}
-        onKeyPress={callback}>
-        <div className="hamburger-box">
-          <div className="hamburger-inner" />
-        </div>
+const Hamburger = ({ open, callback }) => {
+  return (
+    <div
+      role="button"
+      aria-label="open sidebar"
+      tabIndex="0"
+      className={`hamburger ${open ? 'open' : ''}`}
+      onClick={callback}
+      onKeyPress={callback}
+    >
+      <div className="hamburger-box">
+        <div className="hamburger-inner" />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Hamburger;
 
 Hamburger.propTypes = {
   open: PropTypes.bool.isRequired,
